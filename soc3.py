8de5348a96a987c228325aeda97856823af9e309
Himaja <102241159+hima-33378@users.noreply.github.com>
2024-01-12 10:17:03 +0530

    Create soc3.py

---
+++ soc3.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.header('puppy')
+st.write('puppy')
+st.image('https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fa7%2Ff7%2F73%2Fa7f773018836201fb5e6d1e9a24049b8.jpg&tbnid=xredre6p0WNl0M&vet=10CAIQxiAoAGoXChMI4NT_rYXXgwMVAAAAAB0AAAAAEAY..i&imgrefurl=https%3A%2F%2Flakewoodsymphony.org%2Fzeb.aspx%3Fiid%3D408359540%26cid%3D194%26shop%3Dwhite%2Bfluffy%2Bsmall%2Bcute%2Bpuppies%26pr%3D41.99%26g%3D2&docid=g8pcpF7nuoA6CM&w=637&h=614&itg=1&q=puppy%20images&ved=0CAIQxiAoAGoXChMI4NT_rYXXgwMVAAAAAB0AAAAAEAY',caption='puppy')
